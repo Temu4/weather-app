297098b067085747b788a25a24850b4c3825b5c7
Temu4 <artem.vorobjov@gmail.com>
2019-03-02 00:57:15 +0200

    weather app (version 1)

--- src/script.js
+++ src/script.js
@@ -2,6 +2,9 @@
 const sideBar = document.getElementById('sideBar');
 const container = document.getElementById('container');
 const hamburger = document.getElementById('hamburger');
+const main = document.getElementById('main');
+const favoriteList = document.getElementById('favoriteList');
+const historyList = document.getElementById('historyList');
 
 hamburger.addEventListener('click', event => {
   sideBar.classList.toggle('visible');
@@ -10,3 +13,25 @@ hamburger.addEventListener('click', event => {
 });
 
 container.addEventListener('click', () => sideBar.classList.remove('visible'));
+
+//Sidebar buttons
+
+sideBar.addEventListener('click', event => {
+  if (event.target.getAttribute('data-type') === 'btn-main') {
+    favoriteList.classList.add('hidden');
+    historyList.classList.add('hidden');
+    main.classList.remove('hidden');
+  } else if (event.target.getAttribute('data-type') === 'btn-favorite') {
+    main.classList.add('hidden');
+    historyList.classList.add('hidden');
+    favoriteList.classList.remove('hidden');
+  } else if (event.target.getAttribute('data-type') === 'btn-history') {
+    main.classList.add('hidden');
+    favoriteList.classList.add('hidden');
+    historyList.classList.remove('hidden');
+  } else if (event.target.getAttribute('data-type') === 'btn-celcius') {
+    console.log('celcius');
+  } else if (event.target.getAttribute('data-type') === 'btn-fahrenheit') {
+    console.log('fahrenheit');
+  }
+});
